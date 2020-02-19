7f171c846c33201eeb6d5a9554f08960c870abb7
GmoneyAlan <alan200@outlook.com>
2020-02-19 14:44:12 -0700

    Modified exhaustive search

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,2 +1,17 @@
 public class Main {
+    public static void main(String[] args) {
+        boolean[][] snake1 = {
+                {false,false,false,false},
+                {true,false,false,true},
+                {true,false,false,true},
+                {true,true, true, true}
+
+        };
+        SnakeGame snake = new SnakeGame(snake1, 1, 0);
+        int[] alan = snake.findTailExhaustive();
+
+
+        System.out.println("Row:" +alan[0] + " Column:"+ alan[1] + "  Length:" +alan[2]);
+
+    }
 }
--- java/src/SnakeGame.java
+++ java/src/SnakeGame.java
@@ -2,59 +2,79 @@ public class SnakeGame {
 
     private boolean[][] game;;
     private int[] headPosition = new int[2];
-    private static int exhaustiveChecks;
-    private static int recursiveChecks;
+    private static int exhaustiveChecks = 0;
+    private static int recursiveChecks = 0;
 
     public SnakeGame(){
         game = new boolean[1][1];
     }
 
-    public SnakeGame(boolean[][] load, int x, int y){
-        game = new boolean[load.length][load[0].length]
+    public SnakeGame(boolean[][] load, int row, int col){
+        game = new boolean[load.length][load[0].length];
         for(int r = 0; r < load.length; r++){
             for(int c = 0; c < load[r].length; c++){
                 game[r][c] = load[r][c];
             }
         }
-        headPosition[0] = x;
-        headPosition[1] = y;
+        headPosition[0] = row;
+        headPosition[1] = col;
     }
 
     public int[] findTailExhaustive(){
-        
-    }//method end
-    public int[] findTailRecursive(){
+        int[] tPos = {0,0,0};
+        resetCounters();
+        for(int r = 0; r < game.length; r++){
+            for(int c = 0; c < game[0].length; c++){
 
-    }//method end
-    private int[] findTailRecursive(int[] currentPosition, int[] previousPosition){
+                if(game[r][c] && (vecinos(r,c) == 1)){
+                    if((r != headPosition[0]) || c != headPosition[1]){
+                        tPos[0] = c;
+                        tPos[1] = r;
+                    }
+                }
 
+                if(game[r][c]){
+                    tPos[2]++;
+                }
+                recursiveChecks++;
+            }
+        }
+        return tPos;
     }//method end
+    //public int[] findTailRecursive(){
 
-    public int snakebody(int r, int c){
+    //}//method end
+    //private int[] findTailRecursive(int[] currentPosition, int[] previousPosition){
+
+    //}//method end
+
+    public int vecinos(int r, int c){
         int sum = 0;
 
                 if(r - 1> -1 && game[r - 1][c]){
                     sum++;
-                }
+                }//above
                 if(r + 1 < game.length && game[r + 1][c]){
                     sum++;
-                }
+                }//below
                 if(c - 1 > -1 && game[r][c - 1]){
                     sum++;
-                }
-                if(c + 1 < game[r].length && game[r + 1][c]){
+                }//left
+                if(c + 1 < game.length && game[r][c + 1]){
                     sum++;
-                }
+                }//right
         return sum;
     }
     private void resetCounters(){
         recursiveChecks = 0;
         exhaustiveChecks = 0;
     }
-    private static int getRecursiveChecks(){
+    private static int getRecursiveChecks()
+    {
         return recursiveChecks;
     }
     private static int getExhaustiveChecks(){
+
         return exhaustiveChecks;
     }
 
